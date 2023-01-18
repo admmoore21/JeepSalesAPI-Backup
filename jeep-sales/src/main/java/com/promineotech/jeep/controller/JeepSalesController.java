88fa2f33f49af1b63f59f38dc72b757af0c275c0
Adam Moore <112491686+admmoore21@users.noreply.github.com>
2023-01-18 15:13:53 -0600

    Add files via upload
    End of week 15 homework backup

--- jeep-sales/src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
+++ jeep-sales/src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
@@ -1,15 +1,11 @@
 package com.promineotech.jeep.controller;
 
 import java.util.List;
-import javax.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import com.promineotech.jeep.Constants;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -21,12 +17,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@Validated
 @RequestMapping("/jeeps")
 @OpenAPIDefinition(info = @Info(title = "Jeep Sales Service"), servers = {
         @Server(url = "http://localhost:8080", description = "Local server.")})
 public interface JeepSalesController {
-
   // @formatter:off
   @Operation(
       summary = "Returns a list of Jeeps",
@@ -68,8 +62,6 @@ public interface JeepSalesController {
   @ResponseStatus(code = HttpStatus.OK)
   List<Jeep> fetchJeeps(
       @RequestParam JeepModel model,
-      @Length(max = Constants.TRIM_MAX_LENGTH)
-      @Pattern(regexp = "[//w//s]*")
       @RequestParam String trim);
   // @formatter:on}
 }
--- jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,14 +1,12 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,11 +16,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
-import com.promineotech.jeep.Constants;
-import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -33,15 +31,12 @@ import com.promineotech.jeep.entity.JeepModel;
     "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
     config = @SqlConfig(encoding = "utf-8"))
 
-class FetchJeepTest extends FetchJeepTestSupport {
+class FetchJeepTest {
   @Autowired
   private TestRestTemplate restTemplate;
   @LocalServerPort
   private int serverPort;
   
-  /**
-   * 
-   */
   @Test
   void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
    
@@ -49,8 +44,9 @@ class FetchJeepTest extends FetchJeepTestSupport {
     JeepModel model = JeepModel.WRANGLER;
     String trim = "Sport";
     String uri = 
-        String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
-   
+        String.format("http://localhost:%d/jeeps?model=%s&trim=%s", 
+                      serverPort, model, trim);
+    
     //When: A connection is made to the URI
     ResponseEntity<List<Jeep>> response = 
         restTemplate.exchange(uri, HttpMethod.GET, null, 
@@ -66,58 +62,29 @@ class FetchJeepTest extends FetchJeepTestSupport {
     assertThat(actual).isEqualTo(expected);
   }
 
-  @Test
-  void testThatAnErroMessageIsReturnedWhenAnUnknownTrimIsSupplied() {
-   
-    //Given: A valid model trim and URI
-    JeepModel model = JeepModel.WRANGLER;
-    String trim = "Unknown Value";
-    String uri = 
-        String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
+  
+  protected List<Jeep> buildExpected() {
+    List<Jeep> list = new LinkedList<>();
     
-    //When: A connection is made to the URI
-    ResponseEntity<Map<String, Object>> response = restTemplate.exchange(uri, 
-        HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+    // @formatter:off
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(2)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("28475.00"))
+        .build());
     
-    //Then: a not found (404) status
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(4)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("31975.00"))
+        .build());
+    // @formatter:on
     
-    // And: an error message is returned
-    Map<String, Object> error = response.getBody();
-    
-    assertErrorMessageValid(error, HttpStatus.NOT_FOUND);
+    Collections.sort(list);
+    return list;
   }
-
-  @ParameterizedTest
-  @MethodSource("com.promineotech.jeep.controller.FetchJeepTest#parametersForInvalidInput")
-  void testThatAnErroMessageIsReturnedWhenAnInvalidValueIsSupplied(
-      String model, String trim, String reason) {
-   
-    //Given: A valid model trim and URI
-    String uri = 
-        String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
-                     
-    
-    //When: A connection is made to the URI
-    ResponseEntity<Map<String, Object>> response = restTemplate.exchange(
-        uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
-    
-    //Then: a not found (404) status
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-    
-    // And: an error message is returned
-    Map<String, Object> error = response.getBody();
-    
-    assertErrorMessageValid(error, HttpStatus.BAD_REQUEST);
-  }
-
- static Stream<Arguments> parametersForInvalidInput() {
-   // @formatter:off
-   return Stream.of(
-       arguments("WRANGLER", "@#$%$", "Trim contains non-alpha-numberic characters."),
-       arguments("WRANGLER", "C".repeat(Constants.TRIM_MAX_LENGTH + 1), "Trim length too long."),
-       arguments("INVALID", "Sport", "Model is not enum value")
-       // @formatter:on
-       );
- }
 }
